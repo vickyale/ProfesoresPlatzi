a36155d4b1ff9e9b05d9d1179559f3ea48b54d3e
Victoria Ubaldo <viky.ubaldo@gmail.com>
2017-12-10 00:23:22 -0500

    update datasource

--- src/main/java/com/platzi/profesoresplatzi/configuration/DatabaseConfiguration.java
+++ src/main/java/com/platzi/profesoresplatzi/configuration/DatabaseConfiguration.java
@@ -1,9 +1,15 @@
 package com.platzi.profesoresplatzi.configuration;
 
  
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,21 +19,43 @@ public class DatabaseConfiguration {
 
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactoryBean = LocalSessionFactoryBean();
-		sessionFactoryBean.setPackagesToScan("");
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setPackagesToScan("com.platzi.profesores.model");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties());
 		
-	}
-	
-   private LocalSessionFactoryBean LocalSessionFactoryBean() {
-		// TODO Auto-generated method stub
-		return null;
+		return sessionFactoryBean;
 	}
 
-   @Bean
+
+
+@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/platziprofesores");
 		dataSource.setUsername("platziprofesores");
+		dataSource.setPassword("platziprofesores");
+		
+		return dataSource ;
 	}
+   
+   
+   public Properties hibernateProperties() {
+	   Properties properties = new Properties();
+	   properties.put("hibernate.dialect", "hibernate.dialect");
+	   properties.put("show.sql", "true");
+	   
+	   return properties;
+   }
+   
+   
+   @Bean
+   @Autowired
+   public HibernateTransactionManager transactionManager() {
+	   HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+	   hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
+	   
+	   return hibernateTransactionManager ;
+   }
 }
